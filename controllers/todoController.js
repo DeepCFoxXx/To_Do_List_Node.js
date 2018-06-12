3816c9fe3f1689fddfa00cdc6f7bc5ce1c5add9e
DeepCFoxXx <learncode23@gmail.com>
2018-06-12 13:44:28 +0100

    started todo controller.js

---
+++ controllers/todoController.js
@@ -0,0 +1,17 @@
+var bodyParser = require('body-parser');
+
+module.exports = function(app){
+
+  app.get('/todo', function(req, res){
+
+  });
+
+  app.post('/todo', function(req, res){
+
+  });
+
+  app.delete('/todo/:item', function(req, res){
+
+  });
+  
+};
