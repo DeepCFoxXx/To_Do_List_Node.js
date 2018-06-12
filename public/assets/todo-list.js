4fe8c7d626e5dcb6889702efbd7c2a6cbebd5f88
DeepCFoxXx <learncode23@gmail.com>
2018-06-12 13:41:05 +0100

    ajax delete function added

--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -16,4 +16,17 @@ $(document).ready(function(){
     return false;
   });
 
-});  
+  $('li').on('click', function(){
+
+      var item = $(this).text().replace(/ /g, "-");
+
+      $.ajax({
+        type: 'DELETE',
+        url: '/todo/' + item,
+        success: function(data){
+          location.reload();
+        }
+      });
+  });
+
+});
