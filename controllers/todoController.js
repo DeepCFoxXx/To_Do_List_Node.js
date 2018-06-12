571eee6c6ba43390239c93600aea425706fbe0d8
DeepCFoxXx <learncode23@gmail.com>
2018-06-12 14:19:51 +0100

    render data added

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,9 +1,12 @@
 var bodyParser = require('body-parser');
 
+var data = [{item: 'Feed the cat'}, {item: 'Bake pie'}, {item: 'Eat pie then sleep'}];
+
+
 module.exports = function(app){
 
   app.get('/todo', function(req, res){
-
+    res.render('todo', {todos: data});
   });
 
   app.post('/todo', function(req, res){
@@ -13,5 +16,5 @@ module.exports = function(app){
   app.delete('/todo/:item', function(req, res){
 
   });
-  
+
 };
